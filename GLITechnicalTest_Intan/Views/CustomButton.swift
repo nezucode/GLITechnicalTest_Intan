1a907471dda7af3ef0757d162dab065485031a72
nezucode <intann89.in@gmail.com>
2024-01-18 19:00:54 +0700

    changet title button to semibold

--- GLITechnicalTest_Intan/Views/CustomButton.swift
+++ GLITechnicalTest_Intan/Views/CustomButton.swift
@@ -11,6 +11,7 @@ class CustomButton: UIButton {
     init(title: String){
         super.init(frame: .zero)
         self.setTitle(title, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
         self.layer.cornerRadius = 12
         self.layer.masksToBounds = true
         self.layer.backgroundColor = .init(red: 126/255, green: 31/255, blue: 3/255, alpha: 1)
